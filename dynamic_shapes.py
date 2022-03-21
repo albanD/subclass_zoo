4c7e87e3e930526f6c693f38d4d884ac59e4b7ef
Edward Z. Yang <ezyang@fb.com>
2022-03-20 22:33:36 -0400

    publish notebook
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- dynamic_shapes.py
+++ dynamic_shapes.py
@@ -1,4 +1,17 @@
-# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
 import functools
 import itertools
