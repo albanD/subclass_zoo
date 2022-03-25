6938cf554f2825a1dfd4dde7c90d832babb09c97
Edward Z. Yang <ezyang@fb.com>
2022-03-25 12:59:50 -0400

    empty tensor
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ empty_tensor.py
@@ -0,0 +1,54 @@
+from types import FunctionType
+
+import torch
+from base_tensor import BaseTensor
+from torch import Tensor
+from torch.testing._internal.common_utils import run_tests, TestCase
+from torch.utils._pytree import tree_map
+
+from utils import no_dispatch
+
+class EmptyTensor(BaseTensor):
+    @staticmethod
+    def __new__(cls, elem):
+        return torch.Tensor._make_wrapper_subclass(
+            cls, elem.size(),
+            strides=elem.stride(), storage_offset=elem.storage_offset(),
+            dtype=elem.dtype, layout=elem.layout, requires_grad=elem.requires_grad,
+            device=elem.device
+        )
+
+    def __init__(self, elem):
+        pass
+
+    @classmethod
+    def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
+        def inflate(t):
+            if isinstance(t, cls):
+                return torch.ones_like(t, device=t.device)
+            else:
+                return t
+
+        def deflate(t):
+            if isinstance(t, torch.Tensor) and not isinstance(t, cls):
+                return EmptyTensor(t)
+            else:
+                return t
+
+        return tree_map(deflate, super().__torch_dispatch__(
+            func, types,
+            tree_map(inflate, args),
+            tree_map(inflate, kwargs)
+        ))
+
+
+class EmptyTensorTest(TestCase):
+    def test_basic(self):
+        x = EmptyTensor(torch.randn(4))
+        y = EmptyTensor(torch.randn(4))
+        r = x + y
+        print(r)
+
+
+if __name__ == "__main__":
+    run_tests()
