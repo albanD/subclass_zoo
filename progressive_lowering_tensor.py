2ae5e9f9eef6b1a1b887629fd45f7a5e97fd6377
Edward Z. Yang <ezyang@fb.com>
2022-04-19 16:19:01 -0400

    progressive lowering tensor
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ progressive_lowering_tensor.py
@@ -0,0 +1,48 @@
+import torch
+from utils import no_dispatch
+from torch.utils._pytree import tree_map
+from torch.testing._internal.common_utils import run_tests, TestCase
+
+CALLED = []
+
+
+class ProgressiveLoweringTensor(torch.Tensor):
+    @classmethod
+    def wrap(cls, t):
+        if isinstance(t, torch.Tensor) and not isinstance(t, cls):
+            return cls(t)
+        else:
+            return t
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        if func is torch.Tensor.relu:
+            CALLED.append(func)
+            with torch._C.DisableTorchFunction():
+                with no_dispatch():
+                    return tree_map(cls.wrap, func(*args, **kwargs))
+        else:
+            with torch._C.DisableTorchFunction():
+                return func(*args, **kwargs)
+
+    @classmethod
+    def __torch_dispatch__(cls, func, types, args=(), kwargs=None):
+        CALLED.append(func)
+        with no_dispatch():
+            return tree_map(cls.wrap, func(*args, **kwargs))
+
+
+class ProgressiveLoweringTensorTest(TestCase):
+    def test_basic(self):
+        CALLED.clear()
+        x = ProgressiveLoweringTensor(torch.randn(2))
+        x.add(2).relu()
+        # add call is low level aten op; relu call is high level torch
+        # op
+        self.assertEqual(CALLED, [torch.ops.aten.add, torch.Tensor.relu])
+
+
+if __name__ == "__main__":
+    run_tests()
