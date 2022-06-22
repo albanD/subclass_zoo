719a386fe62dc282b41ed05646d2b0ee41eb9f23
sanketpurandare <sanketpurandare@gmail.com>
2022-06-22 01:44:11 +0000

    fixing import issue

--- dispatch_mem_profiler.py
+++ dispatch_mem_profiler.py
@@ -7,7 +7,6 @@ from torch.utils._pytree import tree_map, tree_flatten
 from typing import List, Any
 from numbers import Number
 from collections import defaultdict
-from torchdynamo.testing import reduce_to_scalar_loss
 from torch.utils._python_dispatch import push_torch_dispatch_mode, TorchDispatchMode
 aten = torch.ops.aten
 
@@ -57,20 +56,17 @@ def add_series(series_name):
 def save_graph(filename:str):
     global series, markers
     import matplotlib.pyplot as plt
-    marker_iter = iter(markers.items())
     for series_name, mem_usage in series.items():
         y = mem_usage.values()
         min_val = min(y)
         max_val = max(y)
         x = [i for i in range(len(y))]
         plt.plot(x,y, label=series_name)
-        marker_name, marker = next(marker_iter)
-        plt.plot([marker, marker], [min_val, max_val], 'k-', lw=2, label=marker_name)
     plt.xlabel("# Operator Calls")
     plt.ylabel("Allocated Memory (MB)")
     plt.title(filename)
-    # for marker_name,marker in markers.items():
-    #     plt.plot([marker, marker], [min_val, max_val], 'k-', lw=2, label=marker_name)
+    for marker_name,marker in markers.items():
+        plt.plot([marker, marker], [min_val, max_val], 'k-', lw=2, label=marker_name)
     plt.legend()
     print("Saving Graph")
     plt.savefig("../"+filename)
@@ -81,7 +77,8 @@ def add_marker(marker_name):
     last_val_num = len(mem_usage.values())
     markers[marker_name+str(k)] = last_val_num
 
-
+def reduce_to_scalar_loss(inp):
+    return inp.sum()
 
 from functorch.compile import aot_module, nop, print_compile, min_cut_rematerialization_partition
 
@@ -100,18 +97,19 @@ def mem_profile_model(mod:torch.nn.Module, inp:torch.Tensor):
         loss.backward()
 
 if __name__ == "__main__":
+    
     import torchvision.models as models
     mod = models.resnet18().cuda()   
-    inp = torch.randn(16, 3, 224, 224, device='cuda')
+    inp = torch.randn(32, 3, 224, 224, device='cuda')
     mem_profile_model(mod, inp)
     add_series("eager_mode")
     mod3 = aot_module(mod, nop, partition_fn=min_cut_rematerialization_partition) 
     mem_profile_model(mod3, inp)
     add_series("aot_autograd_min_cut")
     save_graph("Resnet18_mem_usage")
-
-    # with MemoryProfileDispatchMode(True):
-    #     mod3 = aot_module(mod, nop, partition_fn=min_cut_rematerialization_partition) 
-    #     mod3(inp).sum().backward()
-    #     add_series("aot_autograd_mem_usage")
-    #     save_graph("autograd_mem_usage")
+    clear_state()
+    with MemoryProfileDispatchMode(True):
+        mod3 = aot_module(mod, nop, partition_fn=min_cut_rematerialization_partition) 
+        mod3(inp).sum().backward()
+        add_series("aot_autograd_mem_usage")
+        save_graph("autograd_mem_usage")
