a6626abd3de2ebcb7f6d204ea2c357c8c78463b7
sanketpurandare <sanketpurandare@gmail.com>
2022-06-22 22:58:32 +0000

    Added requirements and rcleaned up global usage

--- dispatch_mem_profiler.py
+++ dispatch_mem_profiler.py
@@ -1,40 +1,38 @@
-import functools
-import torch
-
 import torch
 import torch.nn as nn
 from torch.utils._pytree import tree_map, tree_flatten
-from typing import List, Any
-from numbers import Number
+from typing import List, Any, Dict
 from collections import defaultdict
+from functorch.compile import aot_module, nop, print_compile, min_cut_rematerialization_partition
 from torch.utils._python_dispatch import push_torch_dispatch_mode, TorchDispatchMode
+from torchvision import models
 aten = torch.ops.aten
 
 MB = 1024 * 1024.0
 
-operator_names = defaultdict(int)
-mem_usage = defaultdict(float)
-markers = defaultdict(int)
-series = {}
+operator_names:Dict[str,int] = defaultdict(int)
+mem_usage:Dict[str, float] = defaultdict(float)
+markers:Dict[str, int] = defaultdict(int)
+series: Dict[str, Dict[str, float]] = defaultdict(lambda: defaultdict(float))
 
 def normalize_tuple(x):
     if not isinstance(x, tuple):
         return (x,)
     return x
 
+def reduce_to_scalar_loss(inp):
+    return inp.sum()
 
 class MemoryProfileDispatchMode(TorchDispatchMode):
     def __init__(self, verbose=False):
-        self.verbose = verbose
+        self.verbose: bool = verbose
     
     def __torch_dispatch__(self, func, types, args=..., kwargs=None):
-
-        global mem_usage, operator_names
         rs = func(*args, **kwargs)
         if func == torch.ops.aten.detach.default:
             return rs
-        mem = torch.cuda.memory_allocated()/MB
-        func_name = func.__name__ + "_"+ str(operator_names[func.__name__])
+        mem:float = torch.cuda.memory_allocated()/MB
+        func_name:str = func.__name__ + "_"+ str(operator_names[func.__name__])
         operator_names[func.__name__] = operator_names[func.__name__] + 1
         mem_usage[func_name] = mem
         if(self.verbose):
@@ -42,19 +40,17 @@ class MemoryProfileDispatchMode(TorchDispatchMode):
         return rs
 
 def clear_state():
-    global operator_names, mem_usage
     operator_names.clear()
     mem_usage.clear()
 
 def add_series(series_name):
-    global mem_usage, series
+    global mem_usage
     fin_usage = torch.cuda.memory_allocated()/MB
     mem_usage['fin_usage'] = fin_usage
     series[series_name]=mem_usage
     mem_usage = defaultdict(float)
 
 def save_graph(filename:str):
-    global series, markers
     import matplotlib.pyplot as plt
     for series_name, mem_usage in series.items():
         y = mem_usage.values()
@@ -72,15 +68,11 @@ def save_graph(filename:str):
     plt.savefig("../"+filename)
 
 def add_marker(marker_name):
-    global markers, mem_usage, series
     k = len(series.keys())
     last_val_num = len(mem_usage.values())
     markers[marker_name+str(k)] = last_val_num
 
-def reduce_to_scalar_loss(inp):
-    return inp.sum()
 
-from functorch.compile import aot_module, nop, print_compile, min_cut_rematerialization_partition
 
 def mem_profile_model(mod:torch.nn.Module, inp:torch.Tensor):
 
@@ -98,9 +90,8 @@ def mem_profile_model(mod:torch.nn.Module, inp:torch.Tensor):
 
 if __name__ == "__main__":
     
-    import torchvision.models as models
-    mod = models.resnet18().cuda()   
-    inp = torch.randn(32, 3, 224, 224, device='cuda')
+    mod: torch.nn.Module = models.resnet18().cuda()   
+    inp:torch.Tensor = torch.randn(32, 3, 224, 224, device='cuda')
     mem_profile_model(mod, inp)
     add_series("eager_mode")
     mod3 = aot_module(mod, nop, partition_fn=min_cut_rematerialization_partition) 
