84bfc5f64c561d15e5a93afa280045162d869799
Edward Z. Yang <ezyang@fb.com>
2022-07-26 12:36:28 -0700

    autograd monkeypatch example
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ autograd_monkeypatch.py
@@ -0,0 +1,25 @@
+from torch.overrides import TorchFunctionMode
+import torch.nn.functional
+
+class BuggyDropout(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, p=0.5):
+        print("forward")
+        return x
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        print("backward")
+        return grad_output, None
+
+
+class AutogradMonkeypatch(TorchFunctionMode):
+    def __torch_function__(self, func, types, args=(), kwargs=None):
+        if not kwargs:
+            kwargs = {}
+        if func is torch.nn.functional.dropout:
+            return BuggyDropout.apply(*args)
+        return func(*args, **kwargs)
+
+with AutogradMonkeypatch():
+    print(torch.nn.functional.dropout(torch.randn(4)))
