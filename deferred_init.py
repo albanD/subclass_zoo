dfb37efb16977a4e5e9eab8cd71334ec0944d96f
Edward Z. Yang <ezyang@fb.com>
2022-08-05 15:24:09 -0700

    add deferred init
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ deferred_init.py
@@ -0,0 +1,63 @@
+import torch
+from torch._subclasses.fake_tensor import FakeTensor, FakeTensorMode
+from torch.utils._python_dispatch import TorchDispatchMode
+from torch.utils._pytree import tree_map
+from torch.fx.experimental.proxy_tensor import ProxyTensor, PythonKeyTracer, ProxyTorchDispatchMode
+from torch.fx import Graph, GraphModule
+from dataclasses import dataclass
+
+# Limitations:
+#   - initialization cannot refer to external tensors
+#   - parameters are these weird ProxyTensors, should have a custom class for
+#     these placeholders
+#   - DCE is likely not sound, needs to be implemented more carefully by
+#     understanding aliasing relationships
+#   - only top level module is rematerialized
+#   - we lose parameter-ness and requires_grad-ness
+
+def deferred_init(f, *args, **kwargs):
+    fx_tracer = PythonKeyTracer()
+    fx_tracer.graph = Graph(fx_tracer)
+    fx_tracer.root = torch.nn.Module()
+    fx_tracer.tensor_attrs = {}
+    fake_tensor_mode = FakeTensorMode(allow_fallback_kernels=True)
+    proxy_mode = ProxyTorchDispatchMode(fx_tracer, trace_factory_functions=True)
+    with fake_tensor_mode, proxy_mode:
+        r = f(*args, **kwargs)
+        r._deferred = fx_tracer
+        return r
+
+def materialize_module(m):
+    # TODO: handle children
+
+    outputs = []
+
+    def mark_for_materialize(tensors):
+        for k, t in tensors.items():
+            if t is None:
+                continue
+            outputs.append(t.proxy.node)
+
+    mark_for_materialize(m._parameters)
+    mark_for_materialize(m._buffers)
+
+    m._deferred.graph.output(outputs)
+    m._deferred.graph.eliminate_dead_code()  # hmmm
+    recomp = GraphModule(m._deferred.root, m._deferred.graph)
+    results = recomp()
+    results_iter = iter(results)
+
+    def replace_results(tensors):
+        for k, t in tensors.items():
+            if t is None:
+                continue
+            tensors[k] = next(results_iter)
+
+    replace_results(m._parameters)
+    replace_results(m._buffers)
+
+
+m = deferred_init(torch.nn.Linear, 3, 5)
+print(m.weight)
+materialize_module(m)
+print(m.weight)
