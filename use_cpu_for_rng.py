7815955509f12824d2cb7f12587455ce404083f1
Edward Z. Yang <ezyang@fb.com>
2022-10-31 11:19:54 -0700

    Add use CPU for RNG example
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ use_cpu_for_rng.py
@@ -0,0 +1,20 @@
+from torch._dispatch.python import enable_python_dispatcher, no_python_dispatcher
+import torch
+
+@torch.ops.aten.randn.default.py_impl(torch._C.DispatchKey.BackendSelect)
+def randn(size, device=None, **kwargs):
+    with no_python_dispatcher():
+        r = torch.ops.aten.randn.default(size, device='cpu', **kwargs)
+    return r.to(device)
+
+# TODO: do the rest of the random functions
+
+# Hack to apply it globally
+ctx = enable_python_dispatcher()
+ctx.__enter__()
+
+torch.manual_seed(0)
+x = torch.randn(10, device='cpu')
+torch.manual_seed(0)
+y = torch.ops.aten.randn.default([10], device='cuda')
+torch.testing.assert_close(x, y.cpu())
