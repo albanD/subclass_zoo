beb8b211eb0ff2a9e667f5da2c8dfc0af4140d69
albanD <desmaison.alban@gmail.com>
2023-05-14 19:12:04 -0400

    basic stuff, missing: mixed sized weight reduce_scatter in bw dropping autograd buffer

--- test_fsdp.py
+++ test_fsdp.py
@@ -3,34 +3,31 @@ from torch.utils import _pytree as pytree
 from torch.autograd import Function
 from torch import nn
 from torch.nn.utils.parametrize import register_parametrization
-from torch.utils._mode_utils import no_dispatch
-import torchviz
 
 
 ### FSDP PART
-class SlicedTensor(torch.Tensor):
+class BucketedTensor(torch.Tensor):
     @staticmethod
-    def __new__(cls, local_tensor, base_tensor):
+    def __new__(cls, view_tensor, base_tensor):
         kwargs = {}
-        kwargs["device"] = local_tensor.device
-        kwargs["dtype"] = local_tensor.dtype
-        kwargs["layout"] = local_tensor.layout
-        self = torch.Tensor._make_wrapper_subclass(cls, local_tensor.size(), **kwargs)
-        # self = torch.Tensor._make_subclass(cls, local_tensor)
-        self._local_tensor = local_tensor
+        kwargs["device"] = view_tensor.device
+        kwargs["dtype"] = view_tensor.dtype
+        kwargs["layout"] = view_tensor.layout
+        self = torch.Tensor._make_wrapper_subclass(cls, view_tensor.size(), **kwargs)
+        self._view_tensor = view_tensor
         self._base_tensor = base_tensor
-        self._all_slices = None
+        self._all_views = None
         return self
 
     __torch_function__ = torch._C._disabled_torch_function_impl
 
     def __repr__(self):
-        # This is (1) sad that we have to special case (2) Parameter printing is so broken
+        # This is (1) sad that we have to special case as (2) Parameter printing is so broken
         if isinstance(self, nn.Parameter):
             prefix = 'Parameter containing:\n'
         else:
             prefix = ''
-        return prefix + f"Sliced({self._local_tensor}, full_backing_size={self._base_tensor.size()})"
+        return prefix + f"Sliced({self._view_tensor}, full_backing_size={self._base_tensor.size()})"
 
     @classmethod
     def __torch_dispatch__(cls, func, types, args, kwargs=None):
@@ -44,27 +41,34 @@ class SlicedTensor(torch.Tensor):
             else:
                 raise NotImplementedError("NYI")
         else:
-            args = pytree.tree_map_only(SlicedTensor, lambda x: x._local_tensor, args)
+            args = pytree.tree_map_only(BucketedTensor, lambda x: x._view_tensor, args)
             return func(*args, **kwargs)
 
     @staticmethod
     def from_full_tensor(full_w):
-        slices = full_w.unbind()
-        all_slices = tuple(SlicedTensor(s, full_w) for s in slices)
+        assert not torch.is_grad_enabled()
+        slices = full_w.unbind(dim=-1)
+        all_slices = tuple(BucketedTensor(s, full_w) for s in slices)
         for s in all_slices:
-            s._all_slices = all_slices
+            s._all_views = all_slices
         return all_slices
 
 class UnsliceReduceFn(Function):
     @staticmethod
     def forward(ctx, *all_w):
         assert all(w._base_tensor is all_w[0]._base_tensor for w in all_w)
-        return all_w[0]._base_tensor.detach()
+        full_local = all_w[0]._base_tensor.detach()
+        # Assume everyone is involved
+        ctx.idxs = list(range(WORLD_SIZE))
+        out = torch.distributed._functional_collectives.all_gather_tensor(full_local, 0, ctx.idxs,"huh")
+        return out
 
     @staticmethod
     def backward(ctx, gO):
         # gO = td.all_reduce(gO)
-        return gO.unbind()
+        # Should be reduce_scatter but that doesn't work on gloo
+        gO = torch.distributed._functional_collectives.all_reduce(gO, "sum", ctx.idxs)
+        return gO.unbind(-1)
 
 class UnsliceReduceGrad(nn.Module):
     def __init__(self, unslice_state):
@@ -72,11 +76,11 @@ class UnsliceReduceGrad(nn.Module):
         self.unslice_state = unslice_state
 
     def forward(self, w):
-        assert isinstance(w, SlicedTensor)
+        assert isinstance(w, BucketedTensor)
         if len(self.unslice_state) > 0:
             return self.unslice_state[0]
         else:
-            new_full_w = UnsliceReduceFn.apply(*w._all_slices)
+            new_full_w = UnsliceReduceFn.apply(*w._all_views)
             # Clear the state after the backward call
             def clean_state_hook(_):
                 self.unslice_state.pop()
@@ -90,81 +94,131 @@ class Slice(nn.Module):
         self.idx = idx
 
     def forward(self, w):
-        return w[self.idx]
+        out = w.select(-1, self.idx)
+        # Make mod.weight.grad work!
+        # We only have a the local shard of the gradient in here?
+        # out.grad = w.grad_fn.next_functions[self.idx][0].variable.grad
+        return out
 
-def make_fsdp_(mod):
-    # Concat all weights together
+def make_fsdp_(mod, rank):
+    mods = [m for m in mod if isinstance(m, nn.Linear)]
+    # Concat all weights together to bucket them together
     full_w = []
-    for l in mod:
-        full_w.append(l.weight)
+    for l in mods:
+        w = l.weight
+        # Do the sharding by dropping part of the weight locally
+        w = w.chunk(WORLD_SIZE)[rank]
+        full_w.append(w)
+
     with torch.no_grad():
-        full_w = torch.stack(full_w)
-        new_w = SlicedTensor.from_full_tensor(full_w)
+        full_w = torch.stack(full_w, dim=-1)
+        new_w = BucketedTensor.from_full_tensor(full_w)
 
     # Update the weights to the new view
     unslice_state = []
-    for i, l in enumerate(mod):
+    for i, l in enumerate(mods):
         l.weight = nn.Parameter(new_w[i])
 
         register_parametrization(l, "weight", UnsliceReduceGrad(unslice_state), unsafe=True)
         register_parametrization(l, "weight", Slice(i), unsafe=True)
 
-### Quantization part
-class QuantizedTensor(torch.Tensor):
-    @staticmethod
-    def __new__(cls, data, dtype, requires_grad=False):
-        return torch.Tensor._make_wrapper_subclass(cls, data.size(), dtype=dtype)
+# ### Quantization part
+# class QuantizedTensor(torch.Tensor):
+#     @staticmethod
+#     def __new__(cls, data, dtype, requires_grad=False):
+#         return torch.Tensor._make_wrapper_subclass(cls, data.size(), dtype=dtype)
 
-    def __init__(self, data, dtype, requires_grad=False):
-        self._data = data
+#     def __init__(self, data, dtype, requires_grad=False):
+#         self._data = data
 
-    __torch_function__ = torch._C._disabled_torch_function_impl
+#     __torch_function__ = torch._C._disabled_torch_function_impl
 
-    def __repr__(self):
-        # This is (1) sad that we have to special case (2) Parameter printing is so broken
-        if isinstance(self, nn.Parameter):
-            prefix = 'Parameter containing:\n'
-        else:
-            prefix = ''
-        return prefix + f"QuantizedTensor({self._data}, public_dtype={self.dtype})"
+#     def __repr__(self):
+#         # This is (1) sad that we have to special case (2) Parameter printing is so broken
+#         if isinstance(self, nn.Parameter):
+#             prefix = 'Parameter containing:\n'
+#         else:
+#             prefix = ''
+#         return prefix + f"QuantizedTensor({self._data}, public_dtype={self.dtype})"
 
-    @classmethod
-    def __torch_dispatch__(cls, func, types, args, kwargs=None):
-        args, kwargs = pytree.tree_map_only(QuantizedTensor, lambda x: x._data, (args, kwargs or {}))
-        # Always downcast everything to bfloat16 for simplicity
-        if pytree.tree_any_only(torch.Tensor, lambda x: x.dtype is torch.bfloat16, args):
-            args, kwargs = pytree.tree_map_only(torch.Tensor, lambda x: x.to(torch.bfloat16) if x.dtype is torch.float32 else x, (args, kwargs))
-        raw_out = func(*args, **kwargs)
-        out = pytree.tree_map_only(torch.Tensor, lambda x: QuantizedTensor(x, torch.float32), raw_out)
-        return out
+#     @classmethod
+#     def __torch_dispatch__(cls, func, types, args, kwargs=None):
+#         args, kwargs = pytree.tree_map_only(QuantizedTensor, lambda x: x._data, (args, kwargs or {}))
+#         # Always downcast everything to bfloat16 for simplicity
+#         if pytree.tree_any_only(torch.Tensor, lambda x: x.dtype is torch.bfloat16, args):
+#             args, kwargs = pytree.tree_map_only(torch.Tensor, lambda x: x.to(torch.bfloat16) if x.dtype is torch.float32 else x, (args, kwargs))
+#         raw_out = func(*args, **kwargs)
+#         out = pytree.tree_map_only(torch.Tensor, lambda x: QuantizedTensor(x, torch.float32), raw_out)
+#         return out
 
-def quantize_params_(mod):
-    for l in mod:
-        l.weight = nn.Parameter(QuantizedTensor(l.weight.to(torch.bfloat16), torch.float32))
+# def quantize_params_(mod):
+#     for l in mod:
+#         l.weight = nn.Parameter(QuantizedTensor(l.weight.to(torch.bfloat16), torch.float32))
 
 
 ### TESTING
+import torch.distributed as dist
+# This is not imported by default, so we need an explicit import
+import torch.distributed._functional_collectives
+import torch.multiprocessing as mp
+import os
 
-# Quant + FSDP
-mod = nn.Sequential(
-    nn.Linear(2, 2),
-    nn.Linear(2, 2),
-    nn.Linear(2, 2),
-)
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
 
-print(mod)
-print(mod[0].weight)
-print(list(mod[0].parameters()))
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
-quantize_params_(mod)
+def cleanup():
+    dist.destroy_process_group()
 
-print(mod)
-print(mod[0].weight)
-print(list(mod[0].parameters()))
+WORLD_SIZE = 2
 
-make_fsdp_(mod)
+def fsdp_main(idx):
+    def _print(txt):
+        print(f"From {idx}: {txt}")
+    setup(idx, WORLD_SIZE)
+    torch.manual_seed(idx)
+    inp = torch.rand(4, 2)
+    target = torch.randn(4, 2)
 
-print(mod)
-print(mod[0].weight)
-print(list(mod[0].parameters()))
+    torch.manual_seed(42)
+    mod = nn.Sequential(
+        nn.Linear(2, 2),
+        nn.ReLU(),
+        nn.Linear(2, 2),
+        nn.ReLU(),
+        nn.Linear(2, 2),
+    )
+    crit = torch.nn.MSELoss()
+
+    _print(f"init w[0]: {mod[0].weight.mean()}")
+    make_fsdp_(mod, idx)
+    _print(f"fsdp w[0]: {mod[0].weight.mean()}")
+    opt = torch.optim.SGD(mod.parameters(), lr=0.01, momentum=0.9)
+
+    for i in range(4):
+        _print(f"fw w[0]: {mod[0].weight.mean()}")
+        pred = mod(inp)
+        l = crit(pred, target)
+        _print(f"loss: {l}")
+
+        l.backward()
+        if hasattr(mod[0], "parametrizations"):
+            _print(f"grad: {mod[0].parametrizations.weight.original.grad.mean()}")
+        else:
+            _print(f"grad: {mod[0].weight.grad.mean()}")
+
+        opt.step()
+        opt.zero_grad()
+    _print(f"final loss: {l}")
+
+    cleanup()
+
+
+if __name__ == "__main__":
+    mp.spawn(fsdp_main,
+        nprocs=WORLD_SIZE,
+        join=True)
 
